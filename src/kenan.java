790b19afb4e906d4b74f53e79dd290aa145d66de
kenanevni <kenanevni@gmail.com>
2022-08-03 22:19:18 +0300

    my name again again 2

--- src/kenan.java
+++ src/kenan.java
@@ -2,5 +2,6 @@ public class kenan {
     public static void main(String[] args) {
         System.out.println("kenan");
         System.out.println("kenan");
+        System.out.println("kenan");
     }
 }
